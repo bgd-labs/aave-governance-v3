ebb04e348367a4ef4aacb7594ec20bed97739ff7
sendra <polsendra@gmail.com>
2024-01-05 10:18:13 +0100

    fix: uncommented

--- scripts/Payloads/Deploy_PayloadsController.s.sol
+++ scripts/Payloads/Deploy_PayloadsController.s.sol
@@ -129,12 +129,12 @@ abstract contract BaseDeployPayloadsController is GovBaseScript {
         Constants.PAYLOADS_CONTROLLER_SALT
       );
 
-    //    if (addresses.chainId != ChainIds.ETHEREUM) {
-    //      for (uint256 i = 0; i < executors.length; i++) {
-    //        Ownable(address(executors[i].executorConfig.executor))
-    //          .transferOwnership(addresses.payloadsController);
-    //      }
-    //    }
+    if (addresses.chainId != ChainIds.ETHEREUM) {
+      for (uint256 i = 0; i < executors.length; i++) {
+        Ownable(address(executors[i].executorConfig.executor))
+          .transferOwnership(addresses.payloadsController);
+      }
+    }
   }
 }
 
