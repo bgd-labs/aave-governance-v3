97d9c49a84203c42bd9d735e2f45ba563312643f
nisnislevi <nissan@certora.com>
2024-12-16 19:36:32 +0200

    syncing to new library structure

---
+++ security/certora/harness/aave-token-v3/BaseAaveToken.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Context} from 'openzeppelin-contracts/contracts/utils/Context.sol';
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {IERC20Metadata} from 'openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+import {DelegationMode} from './DelegationAwareBalance.sol';
+
+// Inspired by OpenZeppelin Contracts (last updated v4.5.0) (token/ERC20/ERC20.sol)
+abstract contract BaseAaveToken is Context, IERC20Metadata {
+  struct DelegationAwareBalance {
+    uint104 balance;
+    uint72 delegatedPropositionBalance;
+    uint72 delegatedVotingBalance;
+    DelegationMode delegationMode;
+  }
+
+  mapping(address => DelegationAwareBalance) internal _balances;
+
+  mapping(address => mapping(address => uint256)) internal _allowances;
+
+  uint256 internal _totalSupply;
+
+  string internal _name;
+  string internal _symbol;
+
+  // @dev DEPRECATED
+  // kept for backwards compatibility with old storage layout
+  uint8 private ______DEPRECATED_OLD_ERC20_DECIMALS;
+
+  /**
+   * @dev Returns the name of the token.
+   */
+  function name() public view virtual override returns (string memory) {
+    return _name;
+  }
+
+  /**
+   * @dev Returns the symbol of the token, usually a shorter version of the
+   * name.
+   */
+  function symbol() public view virtual override returns (string memory) {
+    return _symbol;
+  }
+
+  function decimals() public view virtual override returns (uint8) {
+    return 18;
+  }
+
+  function totalSupply() public view virtual override returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address account) public view virtual override returns (uint256) {
+    return _balances[account].balance;
+  }
+
+  function transfer(address to, uint256 amount) public virtual override returns (bool) {
+    address owner = _msgSender();
+    _transfer(owner, to, amount);
+    return true;
+  }
+
+  function allowance(address owner, address spender)
+    public
+    view
+    virtual
+    override
+    returns (uint256)
+  {
+    return _allowances[owner][spender];
+  }
+
+  function approve(address spender, uint256 amount) public virtual override returns (bool) {
+    address owner = _msgSender();
+    _approve(owner, spender, amount);
+    return true;
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 amount
+  ) public virtual override returns (bool) {
+    address spender = _msgSender();
+    _spendAllowance(from, spender, amount);
+    _transfer(from, to, amount);
+    return true;
+  }
+
+  function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+    address owner = _msgSender();
+    _approve(owner, spender, _allowances[owner][spender] + addedValue);
+    return true;
+  }
+
+  function decreaseAllowance(address spender, uint256 subtractedValue)
+    public
+    virtual
+    returns (bool)
+  {
+    address owner = _msgSender();
+    uint256 currentAllowance = _allowances[owner][spender];
+    require(currentAllowance >= subtractedValue, 'ERC20: decreased allowance below zero');
+    unchecked {
+      _approve(owner, spender, currentAllowance - subtractedValue);
+    }
+
+    return true;
+  }
+
+  function _transfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual {
+    require(from != address(0), 'ERC20: transfer from the zero address');
+    require(to != address(0), 'ERC20: transfer to the zero address');
+
+    if (from != to) {
+      uint104 fromBalanceBefore = _balances[from].balance;
+      uint104 toBalanceBefore = _balances[to].balance;
+
+      require(fromBalanceBefore >= amount, 'ERC20: transfer amount exceeds balance');
+      unchecked {
+        _balances[from].balance = fromBalanceBefore - uint104(amount);
+      }
+
+      _balances[to].balance = toBalanceBefore + uint104(amount);
+
+      _afterTokenTransfer(from, to, fromBalanceBefore, toBalanceBefore, amount);
+    }
+    emit Transfer(from, to, amount);
+  }
+
+  function _approve(
+    address owner,
+    address spender,
+    uint256 amount
+  ) internal virtual {
+    require(owner != address(0), 'ERC20: approve from the zero address');
+    require(spender != address(0), 'ERC20: approve to the zero address');
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+  function _spendAllowance(
+    address owner,
+    address spender,
+    uint256 amount
+  ) internal virtual {
+    uint256 currentAllowance = allowance(owner, spender);
+    if (currentAllowance != type(uint256).max) {
+      require(currentAllowance >= amount, 'ERC20: insufficient allowance');
+      unchecked {
+        _approve(owner, spender, currentAllowance - amount);
+      }
+    }
+  }
+
+  /**
+   * @dev after token transfer hook, added for delegation system
+   * @param from token sender
+   * @param to token recipient
+   * @param fromBalanceBefore balance of the sender before transfer
+   * @param toBalanceBefore balance of the recipient before transfer
+   * @param amount amount of tokens sent
+   **/
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 fromBalanceBefore,
+    uint256 toBalanceBefore,
+    uint256 amount
+  ) internal virtual {}
+}
